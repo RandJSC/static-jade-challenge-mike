f3c5e7fc7e1c7b5c8b1baa6e4f02b898207e885e
Mike Green <mike.is.green@gmail.com>
2011-10-25 22:34:08 -0400

    Cleaned up nested conditionals, moved pipe-to-jade stuff into a pure-ruby IO.popen block

--- jadify.rb
+++ jadify.rb
@@ -4,43 +4,53 @@
 
 f = ARGV[0]
 if FileTest.exist?(f)
-	html_raw = File.read(f)
-	line_split = html_raw.split("\n")
+	html_raw     = File.read(f)
+	line_split   = html_raw.split("\n")
 	output_lines = line_split.dup
 	
 	line_split.each do |line|
+		# pre-match for a comment
+		next unless line =~ /<!--/
+
 		curr_line = line_split.index(line)
 
-		# pre-match for a comment
-		if line =~ /<!--/
-			# See if comment is an include
-			m = line.match /\<!--\ ?\@(\w+)\ (.+)\ ?--\>/
+		# See if comment is an include
+		m = line.match /\<!--\ ?\@(\w+)\ (.+)\ ?--\>/
 
-			if m.length == 3
-				processor = m[1].strip
-				file = m[2].strip
-				fname = File.join(File.dirname(f), "#{file}.#{processor}")
+		# Skip if we didn't correctly match processor and filename
+		next unless m.length > 2
 
-				if FileTest.exist?(fname)
-					tmpl = File.read(fname)
+		processor = m[1].strip
+		file      = m[2].strip
+		fname     = File.join(File.dirname(f), "#{file}.#{processor}")
 
-					%x{which #{processor}}
+		if FileTest.exist?(fname)
+			tmpl = File.read(fname)
 
-					if $?.exitstatus == 0
-						output = `cat #{fname} | #{processor}`
+			%x{which #{processor}}
+			if $?.exitstatus == 0
+				output = ""
+				IO.popen(processor, 'r+') do |pipe|
+					pipe.puts tmpl
+					pipe.close_write
 
-						output_lines.insert(curr_line + 1, output)
-					else
-						puts "Cannot find processor #{processor} in your $PATH"
-						exit 2
+					while l = pipe.gets
+						output << l
 					end
-				else
-					puts "Cannot find file #{file}.#{processor}"
-					exit 1
 				end
+
+				output_lines.insert(curr_line + 1, output)
+			else
+				puts "Cannot find processor #{processor} in your $PATH"
+				exit 2
 			end
+		else
+			puts "Cannot find file #{file}.#{processor}"
+			exit 1
 		end
 	end
 
 	puts output_lines.join("\n")
 end
+
+# vim: set ts=2 :
